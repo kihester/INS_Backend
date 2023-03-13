678a98022a9b284fca751adb7534ad5d9637037c
Kieran Hester <kieran.hester@netcompany.com>
2023-03-13 15:09:07 +0000

    creation of user controller

---
+++ Controllers/UserController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using static UtilityLibraries.UserLibrary;
+
+namespace INS_Backend.Controllers
+
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+
+        private static readonly string[] firstNames = new[]
+        {
+            "ben", "bob", "billy"
+        };
+
+        private static readonly string[] lastNames = new[]
+        {
+            "smith", "thatch", "roof"
+        };
+
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpGet(Name = "users")]
+        public IEnumerable<User> Get()
+        {
+            // replace with database call
+            return Enumerable.Range(1, 5).Select(index => new User
+            {
+                firstName = firstNames[Random.Shared.Next(firstNames.Length)],
+                lastName = lastNames[Random.Shared.Next(lastNames.Length)],
+                age = Random.Shared.Next(1, 100)
+            })
+          .ToArray();
+        }
+    }
+}
